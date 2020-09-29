3ee4d7269e0902a31fe79d5758903c2c663e1d62
Kevin Stradtman <stradtkt22@gmail.com>
2020-09-29 15:22:00 -0400

    Added some starter code for the Database

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,16 @@ public class Main {
         //Connect to the JDBC jar file to connect to the database
         try {
             Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\databases\\test.db");
+//            prevents the auto commit
+//            conn.setAutoCommit(false);
             Statement statement = conn.createStatement();
-            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+            statement.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)");
+//            statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Joe', 5145541231, 'joe@gmail.com')");
+//            statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Bill', 5145555555, 'bill@gmail.com')");
+//            statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Zoe', 0915541222, 'zoe@gmail.com')");
+//            statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Moe', 5222222222, 'moe@gmail.com')");
+//            statement.execute("UPDATE contacts SET phone=5555555555 WHERE name='Kevin'");
+            statement.execute("DELETE FROM contacts WHERE name='Kevin'");
             statement.close();
             conn.close();
         } catch(SQLException e) {
